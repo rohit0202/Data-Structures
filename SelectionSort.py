f8586e0490ed72ae3a489c5e020e46744816215b
Rohit Dhiman <rohitdhiman0202@gmail.com>
2018-04-11 14:17:05 +0530

    Create SelectionSort.py

---
+++ SelectionSort.py
@@ -0,0 +1,15 @@
+list1 = [34,67,12,89,45]
+
+
+for i in range(len(list1)):
+    
+    min_index =i
+    for j in range(i+1,len(list1)):
+        if(list1[min_index] > list1[j]):
+            min_index = j
+    
+    temp = list1[i]
+    list1[i] = list1[min_index]
+    list1[min_index] = temp
+
+print(list1)    
